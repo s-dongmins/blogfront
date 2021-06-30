509ecc8cb8e57f2b937d9ca93792520f1950b268
= <=>
2021-07-01 05:18:41 +0900

    fix

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ var callAPI = (text) => {
         .then(result => alert(JSON.parse(result).body))
         .catch(error => console.log('error', error));
 }
+
+var scan = () => {
+    fetch("https://jbrnbwq2kb.execute-api.ap-northeast-2.amazonaws.com/dev/scan")
+        .then(response => response.text())
+        .then(result => console.log(JSON.parse(result).body))
+        .catch(error => console.log("error", error));
+}
