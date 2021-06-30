65dc85511b81177ea38bb8bd331d4b839f8e5528
= <=>
2021-07-01 06:17:56 +0900

    scan

--- script.js
+++ script.js
@@ -15,8 +15,26 @@ var callAPI = (text) => {
 }
 
 var scan = () => {
-    fetch("https://jbrnbwq2kb.execute-api.ap-northeast-2.amazonaws.com/dev/scan")
-        .then(response => response.text())
-        .then(result => console.log(JSON.parse(result).body))
+    items = fetch("https://jbrnbwq2kb.execute-api.ap-northeast-2.amazonaws.com/dev/scan")
+        .then(response => response.json())
+        .then(result => {
+            body = JSON.parse(result.body);
+            console.log(body.Items)
+            for (var post of body.Items) {
+                var div = document.createElement("div");
+                div.className = "post"
+                var text = document.createElement("span");
+                text.className = "text"
+                var text_text = document.createTextNode(post.text);
+                var time = document.createElement("span");
+                time.className = "time"
+                var time_text = document.createTextNode(post.time);
+                text.appendChild(text_text);
+                time.appendChild(time_text);
+                div.appendChild(text);
+                div.append(time);
+                document.body.appendChild(div);
+            }
+        })
         .catch(error => console.log("error", error));
 }
